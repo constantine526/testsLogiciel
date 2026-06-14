1413d1b20eb3540d464bc398b30786b48b118cc6
constantine526 <jshc25@yahoo.com>
2026-06-14 14:12:13 +0200

    Ajout du test du constructeur vide

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,9 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class VoitureTest {
 
+ @SpringBootTest
+public class VoitureTest {
+
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoiture() {
+        assertEquals(1, 1);
     }
 
+    @Test
+    void constructeurVide() {
+        Voiture v = new Voiture();
+
+        assertEquals(null, v.getMarque());
+        assertEquals(0, v.getPrix());
+        assertEquals(0, v.getId());
+    }
+}
 }
